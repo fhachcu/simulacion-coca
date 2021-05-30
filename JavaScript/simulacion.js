e86481b1faa0f7eae931a617ae01b8b4f507c50e
Facundo Gramajo <fsgramajo@gmail.com>
2021-05-30 19:11:12 -0300

    se agregan las salidas trimestrales

--- JavaScript/simulacion.js
+++ JavaScript/simulacion.js
@@ -1,21 +1,40 @@
-import {binomial, poisson} from './distribuciones.js';   
+import {binomial, poisson, normal} from './distribuciones.js';   
 
 
 let derrame = 0;
+let derrameMensual = 0;
+let binomialAux;
+
+const datos = {
+    derrame:0,
+    latasAllenar:0,
+    perdidaJarabeDinero:0,
+    ventaLatas: 0
+}
 
 export function recorrerMeses(){
+
+    let costoTotalLata = 0;
 
     for(let i=0; i<3; i++){
         
         recorrerDias();
-        
-        
-        
+        let costoDerramemMensual = Math.round(normal(17,3),-2);
+        costoTotalLata = costoTotalLata +(costoDerramemMensual*(derrameMensual/354));
+           
     }
-    return ((derrame/354));
+
+    datos.derrame = Math.round(derrame,-2);
+    datos.latasAllenar = Math.trunc(derrame/354);
+    datos.perdidaJarabeDinero = Math.round(costoTotalLata,-2);
+    datos.ventaLatas = datos.latasAllenar * 60; 
+    return datos;
 }
 
 function recorrerDias(){
+    
+    derrameMensual = 0;
+
     for(let i=0; i<20; i++){
         recorrerHoras();
     }
@@ -31,7 +50,9 @@ function recorrerMinutos(){
     for(let i=0; i<60; i++){
         let latas = poisson(1800);
         for(let x = 0; x<latas; x++){
-            derrame = derrame + binomial();
+            let binomialAux = binomial();
+            derrame = derrame + binomialAux;
+            derrameMensual = derrameMensual + binomialAux;
         }
     }
 }
